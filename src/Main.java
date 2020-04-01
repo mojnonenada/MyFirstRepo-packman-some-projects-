1d057fb4fa9a47caddfe5f25310a42bc50ce4955
mojnonenada <artur-tulinov@mail.ru>
2020-04-02 02:42:59 +0300

    ------------------

--- src/Main.java
+++ src/Main.java
@@ -1,74 +1,25 @@
-import java.net.SocketTimeoutException;
+import java.util.LinkedHashMap;
 
 public class Main {
 
-    public static void main(String[] args)
-    {
-        byte p = 1;
-        short s = 2;
-        int i = 10;
-        long l = 1000000;
-        float f = 700.53f;
-        double du = 123.222;
-        char ch = '@';
-        boolean bool = false;
-        //вывод методов
-        System.out.println(task3(1,1,1,1));
-        System.out.println(task4(5,7));
-        task5(16);
-        System.out.println(task6(-100));
-        task7("Артур");
-        task8(2020);
-    }
-
-    public static int task3(int a, int b, int c, int d)
-    {
-        return a * (b + (c / d));
-    }
-
-    public static boolean task4(int a1, int a2)
-    {
-        if ((a1 + a2 >= 10) && (a1 + a2 <= 20)) {
-            return true;
-        } else {
-            return false;
+    public static void main(String[] args) {
+        String[] names = {"Anatoliy","Anton","Arkadiy","Artur","Boris","Vadim","Valentin","Valeriy","Viktor","Vitaliy"," Vladimir","Artur","Artur","Artur","Artur","Anton","Anton","Anton","Kirill","Maksim","Anatoliy","Anatoliy"};
+        LinkedHashMap<String, Integer> hm = new LinkedHashMap<>();
+        for (String i : names) {
+           Integer kol = hm.get(i);
+           hm.put(i, kol == null ? 1 : (kol+1) );
         }
+        System.out.println("Task 1");
+        System.out.println(hm);
+        System.out.println("Task 2");
+        PhoneBook phoneBook = new PhoneBook();
+        phoneBook.add("Ivanov","79954443553");
+        phoneBook.add("Petrov","79545454553");
+        phoneBook.add("Kofffffff","799533333");
+        phoneBook.add("Ivanov","7323233332");
+        phoneBook.add("ggggggg","77777777777");
+        phoneBook.add("","");
+        System.out.println(phoneBook.get("Petrov"));
     }
-
-    public static void task5(int x)
-    {
-        if (x >= 0) {
-            System.out.println("Число положительное");
-        } else {
-            System.out.println("Число отрицательное");
-        }
-    }
-
-    public static boolean task6(int x1) {
-
-        if (x1 < 0) {
-            return true;
-        }
-        return false;
-    }
-
-    public static void task7 (String name )
-    {
-            System.out.println("Привет, " + name);
-    }
-
-    public static void task8 (int year)
-    {
-        if ((year % 400 ==0) || ((year % 4 == 0) && (year % 100 !=0 ))) {
-            System.out.println("Год високосный");} else
-                {
-                    System.out.println("Год невисокосный");
-
-                }
-    }
-
 }
-
-
-
 
